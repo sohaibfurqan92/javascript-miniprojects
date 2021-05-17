5aa8fe1c86b1d3d5b635f22fdac5c9fb08fbc7b8
sohaibfurqan92 <info@sohaibfurqan.com>
2021-05-17 13:26:06 +0500

    Used classList.toggle to get same effect

--- navbar/app.js
+++ navbar/app.js
@@ -8,9 +8,11 @@ const toggleBtn = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 toggleBtn.addEventListener("click", function () {
-  if (links.classList.contains("show-links")) {
-    links.classList.remove("show-links");
-  } else {
-    links.classList.add("show-links");
-  }
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  //   }
+
+  links.classList.toggle("show-links");
 });
