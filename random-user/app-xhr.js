ae3c6b02e943e48c4affb69763da262bf78f9b32
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-05-27 20:04:57 +0500

    Modified to use promises

--- random-user/app-xhr.js
+++ random-user/app-xhr.js
@@ -1,6 +1,9 @@
 // this project uses the random person api at https://randomuser.me
-import fetchUser from './utils/fetchUser.js';
+//import fetchUser from './utils/fetchUser.js';
+import fetchUser from './utils/fetchUser-promise.js';
 import { getElement } from './utils/getElement.js';
+import displayUser from './utils/displayUser.js';
+
 const url = 'https://randomuser.me/api/';
 
 const btn = getElement('.btn');
@@ -8,7 +11,21 @@ const btn = getElement('.btn');
 function showUser() {
   //fetch random user
   // display user
-  fetchUser(url);
+  let user = '';
+  fetchUser(url).then((data) => {
+    user = data;
+    const { first, last } = user.name;
+    const { large: image } = user.picture;
+    const { email, location, cell: phone } = user;
+    const { age } = user.dob;
+    const { password } = user.login;
+    const address = `${location.street.number} ${location.street.name}, ${location.city}, ${location.state}, ${location.country}`;
+    const name = `${first} ${last}`;
+
+    const userData = { name, image, email, address, phone, age, password };
+
+    displayUser(userData);
+  });
 }
 
 btn.addEventListener('click', showUser);
