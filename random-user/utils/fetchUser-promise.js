b7fc1c1d37b1c1bc3c9e6acbde43f70425f5e4b1
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-05-27 20:06:19 +0500

    Added fetchUser module with promises

---
+++ random-user/utils/fetchUser-promise.js
@@ -0,0 +1,31 @@
+function fetchUser(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+
+  return new Promise((resolve, reject) => {
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const user = JSON.parse(xhr.responseText).results[0];
+        resolve(user);
+
+        // const { first, last } = user.name;
+        // const { large: image } = user.picture;
+        // const { email, location, cell: phone } = user;
+        // const { age } = user.dob;
+        // const { password } = user.login;
+        // const address = `${location.street.number} ${location.street.name}, ${location.city}, ${location.state}, ${location.country}`;
+        // const name = `${first} ${last}`;
+
+        // const userData = { name, image, email, address, phone, age, password };
+
+        // displayUser(userData);
+      } else {
+        return;
+      }
+    };
+
+    xhr.send();
+  });
+}
+
+export default fetchUser;
