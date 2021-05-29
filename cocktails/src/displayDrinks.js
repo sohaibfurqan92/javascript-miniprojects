21900491fadb8867c796bbd8f7f913d4ca32ef40
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-05-29 13:09:52 +0500

    Display drinks or not found string

---
+++ cocktails/src/displayDrinks.js
@@ -0,0 +1,20 @@
+const displayDrinks = async (section, title, drinks) => {
+  if (!drinks) {
+    section.innerHTML = '';
+    title.textContent = 'Your requested drink was not found! Please try again';
+    return;
+  }
+
+  section.innerHTML = '';
+
+  drinks.forEach((drink) => {
+    section.innerHTML += `<a href="drink.html">
+          <article class="cocktail" data-id="1">
+            <img src="${drink.strDrinkThumb}" alt="cocktail" />
+            <h3>${drink.strDrink}</h3>
+          </article>
+        </a>`;
+  });
+};
+
+export default displayDrinks;
