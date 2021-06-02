2ff08020b0066d70da94b7876fc1b16b5d144f0d
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-06-02 18:47:30 +0500

    Fetched and displayed followers data

--- pagination/app.js
+++ pagination/app.js
@@ -1,4 +1,15 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from './fetchFollowers.js';
+import displayFollowers from './displayFollowers.js';
+import paginate from './paginate.js';
+import displayButtons from './displayButtons.js';
+
+const heading = document.querySelector('h1');
+window.addEventListener('load', init);
+
+async function init() {
+  const followers = await fetchFollowers();
+  if (followers) {
+    heading.textContent = 'pagination';
+  }
+  displayFollowers(followers);
+}
--- pagination/displayFollowers.js
+++ pagination/displayFollowers.js
@@ -1,3 +1,16 @@
-const display = () => {}
+const display = (followers) => {
+  const container = document.querySelector('.container');
 
-export default display
+  container.innerHTML = '';
+  followers.forEach((follower) => {
+    const { avatar_url, login, html_url } = follower;
+
+    container.innerHTML += `<article class="card">
+    <img src=${avatar_url} alt="${login}"/>
+    <h4>${login}</h4>
+    <a href="${html_url}" class="btn">View Profile</a>
+    </article>`;
+  });
+};
+
+export default display;
