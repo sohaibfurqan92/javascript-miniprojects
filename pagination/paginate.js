8596f2a2042bc7266f1af3e4309a9d14af635c40
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-06-03 13:37:53 +0500

    create pages

--- pagination/paginate.js
+++ pagination/paginate.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 10;
 
-export default paginate
+  const followerPages = [];
+
+  //create multiple arrays from one array
+
+  for (let i = 0; i < followers.length; i += itemsPerPage) {
+    followerPages.push(followers.slice(i, i + itemsPerPage));
+  }
+  return followerPages;
+};
+
+export default paginate;
